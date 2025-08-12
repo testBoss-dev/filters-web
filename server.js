f5938d81c101e52d0f0db6678823293df42f8590
Test Boss <disprin.suport@gmail.com>
2025-08-12 18:51:18 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname)));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
